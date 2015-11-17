6d7648e296bbb19b948e288b690774658070ecac
John L <john.m.lenehan@gmail.com>
2015-11-17 15:32:36 -0500

    Working up to parts of Users

--- routes/wiki.js
+++ routes/wiki.js
@@ -7,7 +7,7 @@ var User = models.User;
 router.get("/", function(req, res, next) {
   Page.find({}).exec()
     .then(function(pages) {
-      console.log(pages);
+      //console.log(pages);
       res.render("index", { pages: pages });
     });
 });
@@ -15,11 +15,15 @@ router.get("/", function(req, res, next) {
 router.post("/", function(req, res, next) {
   var title = req.body.title;
   var content = req.body.content;
+  var userProps = {name: req.body.name, email: req.body.email};
+
+  var user = User.findOrCreate(userProps);
   // var status = req.body.status;
   // var author = req.body.author;
   var page = new Page({
     title: title,
-    content: content
+    content: content,
+    tags: req.body.tags
   });
 
   page.save()
@@ -36,11 +40,29 @@ router.get("/add", function(req, res, next) {
   res.render("addpage", locals);
 });
 
+router.get("/search",function(req,res,next){
+  var tags = req.query.tags;
+  // res.render("search")
+  if(tags === ""||tags === undefined){
+  res.render("search",{pages: ""});
+  }else{
+    console.log(tags);
+    tags = tags.split(" ");
+    Page.find({tags: {$in: tags}}).exec()
+    .then(function(values){
+      res.render("search",{pages: values});
+    })
+  }
+
+})
+
+
+
 router.get("/:urlTitle",function(req,res,next){
   Page.findOne({urlTitle: req.params.urlTitle}).exec()
     .then(function(value){
       res.render("wikipage",value);
-      console.log(value);
+      //console.log(value);
     })
     .then(null, function(err) {
       console.error(err);
